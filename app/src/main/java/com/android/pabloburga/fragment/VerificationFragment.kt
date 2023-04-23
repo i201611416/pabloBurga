97abf98338e1f21997ca3e0fca2fdb4da4f9ffaa
jcortezd <joseldcortez@bcp.com.pe>
2023-04-22 23:54:32 -0500

    //

--- app/src/main/java/com/android/pabloburga/fragment/VerificationFragment.kt
+++ app/src/main/java/com/android/pabloburga/fragment/VerificationFragment.kt
@@ -1,12 +1,10 @@
 package com.android.pabloburga.fragment
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.android.pabloburga.databinding.FragmentVerificationBinding
 import com.android.pabloburga.toast
 import com.google.firebase.auth.FirebaseAuth
